16ec17d300ec70db4da0da9baa5c4bdde50d6a99
Dawid Pereira <pereiradawid@outlook.com>
2025-05-21 22:53:47 +0200

    Replaced custom label by tui realm label

--- ui/src/components/mod.rs
+++ ui/src/components/mod.rs
@@ -1,7 +1,8 @@
 pub mod common;
-pub mod label;
+pub mod global_key_watcher;
 pub mod message_details;
 pub mod messages;
-pub mod queue_picker;
 pub mod namespace_picker;
-pub mod global_key_watcher;
+pub mod queue_picker;
+pub mod text_label;
+
